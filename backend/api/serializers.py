a97f19001020a683a48d38b4d15c6edcbdd700ea
Silva137 <goncalosilva17@gmail.com>
2024-12-05 18:02:07 +0000

    authentication

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,13 +1,33 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
-class UserSerializer(serializers.ModelSerializer):
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(write_only=True, required=True)
+    email = serializers.EmailField(required=True)
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'password']
+        fields = ['id', 'username', 'email', 'password', 'password2']
         extra_kwargs = {'password': {'write_only': True}}
 
-    def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+    #Custom logic for serializer.save()
+    def create(self, validate_data):
+        validate_data.pop('password2')
+        user = User.objects.create_user(**validate_data)
         return user
 
+        # Validating Password and Confirm Password while Registration
+
+    def validate(self, attrs):
+        password = attrs.get('password')
+        password2 = attrs.get('password2')
+        if password != password2:
+            raise serializers.ValidationError("Password and Confirm Password doesn't match")
+
+        # Check if the email is already registered
+        email = attrs.get('email')
+        if User.objects.filter(email=email).exists():
+            raise serializers.ValidationError("Email is already registered.")
+
+        return attrs
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('', views.main),
-    path('register/', views.register),
-    path('login/', views.login),
+    path('ask_gpt/', views.ask_gpt),
+    path('auth/register', views.register),
+    path('auth/login', TokenObtainPairView.as_view()),
+    path('auth/refresh', TokenRefreshView.as_view()),
+    path('auth/logout/', views.logout),
+    path('api-auth/', include('rest_framework.urls')),
+
 ]
