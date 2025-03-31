62d706a2211a29f55e5d0620b16a31e77a1754b3
Silva137 <goncalosilva17@gmail.com>
2025-03-31 23:38:47 +0100

    sidebar almost finished - logout button left

--- code/frontend/src/Components/Sidebar/SideBar.tsx
+++ code/frontend/src/Components/Sidebar/SideBar.tsx
@@ -1,20 +1,25 @@
-import { Link } from "react-router-dom";
+import {NavLink, useNavigate } from "react-router-dom";
 import {navItems} from "./SideBarData.jsx";
 import "./Sidebar.css";
+import logo from "../../assets/logo.png"
 
 const Sidebar = () => {
     return (
         <div className="sidebar">
             <div className="sidebar-header">
-                <img src="../../" alt="Logo" className="sidebar-logo" />
+                <img src={logo} alt="Logo" className="sidebar-logo" />
                 <span className="sidebar-title">LLMSentinel</span>
             </div>
             <nav className="sidebar-menu">
                 {navItems.map((item, index) => (
-                    <Link to={item.link} className="menu-item" key={index}>
+                    <NavLink
+                        to={item.link}
+                        className={({ isActive }) =>
+                            isActive ? "menu-item active" : "menu-item"
+                        } key={index}>
                         {item.icon}
                         <span>{item.text}</span>
-                    </Link>
+                    </NavLink>
                 ))}
             </nav>
         </div>
